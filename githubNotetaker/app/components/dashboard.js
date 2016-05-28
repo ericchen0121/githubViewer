8ac400f3532c9f6df65d7e7ab4b36931658fc4ca
Eric Chen <ericchen0121@Erics-MBP.lan>
2016-05-28 01:12:55 -0700

    add in next route with image and touchable highlights

--- githubNotetaker/app/components/dashboard.js
+++ githubNotetaker/app/components/dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableHighlight } from 'react-native';
 
 var styles = StyleSheet.create({
 	container: {
@@ -16,11 +16,57 @@ var styles = StyleSheet.create({
 		alignSelf: 'center'
 	}
 })
+
 class Dashboard extends Component{
+	makeBackground(btn) {
+		var obj = {
+			flexDirection: 'row',
+			alignSelf:'stretch',
+			justifyContent: 'center',
+			flex: 1
+		}
+
+		if(btn === 0) {
+			obj.backgroundColor = '#48BBEC'
+		} else if(btn === 1 ){
+			obj.backgroundColor = '#E77AAE'
+		} else if(btn === 2 ){
+			obj.backgroundColor = '#758BF4'
+		}
+
+		return obj;
+	}
+	goToProfile(){
+		console.log('go to profile')
+	}
+	goToRepos(){
+		console.log('go to profile')
+	}
+	goToNotes(){
+		console.log('go to profile')
+	}
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text> {JSON.stringify(this.props.userInfo)} </Text>
+				<Image source={{uri: this.props.userInfo.avatar_url }} style={styles.image} />
+				<TouchableHighlight
+					style={this.makeBackground(0)}
+					onPress={this.goToProfile.bind(this)}
+					underlayColor='#88D4F5'>
+					<Text style={styles.buttonText}> View Profile</Text>
+				</TouchableHighlight>
+				<TouchableHighlight
+					style={this.makeBackground(1)}
+					onPress={this.goToRepos.bind(this)}
+					underlayColor='#88D4F5'>
+					<Text style={styles.buttonText}> View Repos</Text>
+				</TouchableHighlight>
+				<TouchableHighlight
+					style={this.makeBackground(2)}
+					onPress={this.goToNotes.bind(this)}
+					underlayColor='#88D4F5'>
+					<Text style={styles.buttonText}> View Notes</Text>
+				</TouchableHighlight>
 			</View>
 		)
 	}
